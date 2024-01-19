ce25d369e26f7c127733911a2aa94299f660b750
Julian Monroy <julianmonroy924@gmail.com>
2024-01-19 15:31:05 -0500

    Deleted Class and Included loop for size check

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 //1. I am defining a class to group all corresponding atributes.
 
-class InventoryItem {
-    constructor(ProductName, model, cost, quantity) {
+// class InventoryItem {
+//     constructor(ProductName, model, cost, quantity) {
         
-        this.ProductName = ProductName;
-        this.model = model;
-        this.cost = cost;
-        this.quantity = quantity;
-}
-}
+//         this.ProductName = ProductName;
+//         this.model = model;
+//         this.cost = cost;
+//         this.quantity = quantity;
+// }
+// }
 
 //2. I am creaing an empty array to store the items.
 let inventory = [];
@@ -23,7 +23,7 @@ let productName = prompt('Please enter the new product`s name. (or type `exit` t
 
 if (productName.toLowerCase() === 'exit') {
     break;
-}
+};
 
 //4. I am gathering the next data points for the coresponding product. 
 let modelName = prompt("Enter the Model Number for " + productName + ":");
@@ -31,11 +31,45 @@ let costAmount = prompt("Enter the Cost in dollars and cents (ex00.00) for " + p
 let quantityAvailable = prompt("Enter in Kilograms the Quantity Available  for " + productName + ":");
 
 //5. I am creating an occurence //dont really understand this part//
-let inventoryItem = new InventoryItem(productName, modelName, costAmount, quantityAvailable);
+//  let inventoryItem = new InventoryItem(productName, modelName, costAmount, quantityAvailable);
 
 //6. I am adding set of data objects to the inventory
-inventory.push(inventoryItem);
+
+    inventory.push([productName, modelName, costAmount, quantityAvailable]);
 }
+ //console.log(inventory);
+
+
+ for (let counter = 0; counter < inventory.length; counter++ ) {
+     
+    console.log( inventory [counter]);
+
+ }
+  
 
 //7. I am loging the result
-console.log("Inventory:", inventory);
+// console.log(inventory [1][1]);
+
+// //const bodyElement = document.dody;
+// const arrayContainer = document.createElement("div");
+// //newText.classList.add("h-screen", "flex", "justify-center", "items-center");
+// inventory.forEach(item => {
+//     const spanElement = document.createElement("span");
+
+//     spanElement.className = "inline-block bg-blue-500 text-white px-2 py-1 rounded mr-2";
+
+//     spanElement.textContent = item;
+
+//     // Append the span element to the container
+//     arrayContainer.appendChild(spanElement);
+// });
+
+
+
+
+
+
+
+
+
+// bodyElement.appendChild(newText);
