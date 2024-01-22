51c024f84eee5263ee7cb638c8de97c610d7b992
Julian Monroy <julianmonroy924@gmail.com>
2024-01-22 14:37:43 -0500

    Displayed array in browser, alert

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ while (true) {
     if (productName.toLowerCase() === 'exit') {
         break;
     };
+    //------------------This for loop can be used to replace the while loop. It does not have the exit "break"; rather the user is asked the prompts 
+    //                  how ever many times the counter is allowed to run.(in this example is 2)
+    // for(let counter = 2; counter > 0; counter--){
+    //     // stuff that happens as long as this is true
+    //      // console.log(`the counter is now: ${counter}`)
+    //      // 3. Ask user for stuff? 
+    //      // concept: `prompt`  < ask info from the user
+    //      const productName = prompt("What is the product name?")
+    //      const productModel = prompt("What is the product model number?")
+    //      const productCost = prompt("What is the product cost?")
+    //      const productQuantity = prompt("How many of this product?")
+    //      // add the every new product to the array
+    //      products.push([productName, productModel, productCost, productQuantity])
+    //    }
+    //-------------------
 
 //3. I am gathering the next data points for the coresponding product. 
     let modelName = prompt("Enter the Model Number for " + productName + ":");
@@ -29,27 +44,64 @@ for (let counter = 0; counter < inventory.length; counter++) {
     console.log(inventory[counter]);
 }
 //6. I am displaying the array through an alert utilizing a loop and counter to go through the array.
+let result2 = 0;
+let result = 0;
 for (let counter = 0; counter < inventory.length; counter++ ) {
-    
-    alert(`The catalog contains product # ${counter + 1} `);
-    alert(`ProductName: ${inventory[counter][0]} Model #: ${inventory[counter][1]} Cost: ${inventory[counter][2]} Quantity Added:', ${inventory[counter][3]}`);
+    result += `\nThe catalog contains product # ${counter + 1} \nProductName: ${inventory[counter][0]} Model #: ${inventory[counter][1]} Cost: ${inventory[counter][2]} Quantity Added: ${inventory[counter][3]}\n `;
+
+    result2 += `<br>The catalog contains product # ${counter + 1} <br>ProductName: ${inventory[counter][0]} Model #: ${inventory[counter][1]} Cost: ${inventory[counter][2]} Quantity Added: ${inventory[counter][3]}\n `;
+    //alert(`The catalog contains product # ${counter + 1} `);
+    //alert(`ProductName: ${inventory[counter][0]} Model #: ${inventory[counter][1]} Cost: ${inventory[counter][2]} Quantity Added: ${inventory[counter][3]}`);
     // alert('Model #:', inventory[counter][1]);
     // alert('Cost:', inventory[counter][2]);
     // alert('Quantity:', inventory[counter][3]);
 
 
-//5. I am adding an alert with the final answer
+//-------------------------- This for loop is an automatic scan of all items in an array (all the products in the products?!) It replaces #6 !
+    // for(let product of products){
+     //    ---         --
+    //     console.log(`Product name: ${product[0]}`);
+    //     console.log(`Model Number: ${product[1]} `);
+    //     console.log(`Cost: $ ${product[2]}`);
+    //     console.log(`Qty in Stock: ${product[3]}`);
+      
+    //     // ==== 
+      
+    //     console.log("----------------");
+      
+    //   }
+//-------------------------------
+
+// I am adding an alert with the final answer. (This is a MANUAL way to output the information)
 // alert(`The catalog contains the following item: `);
 // alert(`Product Number 1: ${inventory[counter][0]}, Model #: ${inventory[0][1]}, Cost: ${inventory[0][2]}, Quantity Available: ${inventory[0][3]} `);
 // alert(`Product Number 2: ${inventory[counter][1]}, Model #: ${inventory[1][1]}, Cost: ${inventory[1][2]}, Quantity Available: ${inventory[1][3]} `);
 // alert(`Product Number 3: ${inventory[counter][2]}, Model #: ${inventory[2][1]}, Cost: ${inventory[2][2]}, Quantity Available: ${inventory[2][3]} `);
 // alert(`Product Number 3: ${inventory[counter][3]}, Model #: ${inventory[2][1]}, Cost: ${inventory[2][2]}, Quantity Available: ${inventory[2][3]} `);
 
+
 }
 
+alert(result);
+// I am loging the result in the browser
+//
+let bodyElement = document.dody;
+let divElement = document.createElement("div");
+divElement.classList.add("h-screen", "flex", "justify-center", "items-center");
+divElement.textContent = `Your content here ${inventory}`;
 
-//7. I am loging the result
-// console.log(inventory [1][1]);
+
+divElement.innerHTML = `
+
+<div class="bg-teal-300 p-8 w-2/3 rounded-lg">
+    <p class="text-gray-600 text-xs uppercase"> Catalog MVP by @JulianMonroy </p>
+    <p class="text-4xl"> Catalog: <span class="font-bold text-red"> ${result2} <br> 
+    </span> </p>
+</div>
+`;
+document.body.appendChild(divElement);
+bodyElement.appendChild(divElement);
+
 
 // const bodyElement = document.dody;
 // const arrayContainer = document.createElement("div");
